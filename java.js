5d77c2f00045a4a2521c4ccb9370b79ccd0a1c41
Abdumalik606 <abdumaliksaidjalolov2@gmail.com>
2024-11-11 17:32:52 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+let a = prompt('Tanlang\nA\nB\nC')
+
+if(a === 'A'){
+    console.log('Siz zorsiz');
+}else if(a === 'B'){
+    console.log('Siz yahshi oqiysiz');
+}else if(a === 'C'){
+    console.log('Siz yomon oqiysiz');
+}else{
+    console.log('Siz kaledorda oqiysiz');
+}
+
+
+
+
+
+
+
